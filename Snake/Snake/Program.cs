ce8c73778c23b6ea4fe17ff0373eeb68d1b4f403
ValentinaKozak <valentina.m.kozak@gmail.com>
2017-06-14 16:03:44 +0300

    creation horizontal and vertical lines

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -17,41 +17,12 @@ namespace Snake
             p2.Draw();
 
             Console.WriteLine();
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
 
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
+            HorizontalLine lineH = new HorizontalLine(5, 10, 8, '+');
+            lineH.Draw();
 
-            foreach (int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            numList.RemoveAt(0);
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p1);
-
-            Point t1 = new Point(10, 10, '@');
-            Point t2 = new Point(30, 10, '#');
-            Point t3 = new Point(10, 20, '$');
-            Point t4 = new Point(30, 20, '%');
-
-            List<Point> myListPoint = new List<Point>();
-            myListPoint.Add(t1);
-            myListPoint.Add(t2);
-            myListPoint.Add(t3);
-            myListPoint.Add(t4);
-
-            foreach (Point i in myListPoint)
-            {
-                i.Draw();
-            }
+            VerticalLine lineV = new VerticalLine(12, 5, 10, '$');
+            lineV.Draw();
 
             Console.ReadLine();
         }
